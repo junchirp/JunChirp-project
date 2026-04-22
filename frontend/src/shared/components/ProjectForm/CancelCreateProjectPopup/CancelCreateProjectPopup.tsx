b6f91ca1cb5baa7180e43660353da74cf93a6e26
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2026-04-22 18:10:30 +0300

    update cancel create project popup

--- frontend/src/shared/components/ProjectForm/CancelCreateProjectPopup/CancelCreateProjectPopup.tsx
+++ frontend/src/shared/components/ProjectForm/CancelCreateProjectPopup/CancelCreateProjectPopup.tsx
@@ -1,42 +1,38 @@
 'use client';
 
 import { ReactElement } from 'react';
-import styles from './CancelCreateProjectPopup.module.scss';
 import Button from '@/shared/components/Button/Button';
 import { useTranslations } from 'next-intl';
+import Dialog from '@/shared/components/Dialog/Dialog';
+import DialogHeader from '@/shared/components/Dialog/DialogHeader/DialogHeader';
+import DialogBody from '@/shared/components/Dialog/DialogBody/DialogBody';
+import DialogFooter from '@/shared/components/Dialog/DialogFooter/DialogFooter';
 
 interface CancelCreateProjectPopupProps {
   onCancel: () => void;
   onConfirm: () => void;
+  isOpen: boolean;
 }
 
 export default function CancelCreateProjectPopup(
   props: CancelCreateProjectPopupProps,
 ): ReactElement {
-  const { onCancel, onConfirm } = props;
+  const { onCancel, onConfirm, isOpen } = props;
   const tButtons = useTranslations('buttons');
   const tPopup = useTranslations('cancelCreateProjectPopup');
 
   return (
-    <div className={styles['cancel-create-project-popup__wrapper']}>
-      <div className={styles['cancel-create-project-popup']}>
-        <div className={styles['cancel-create-project-popup__content']}>
-          <h3 className={styles['cancel-create-project-popup__title']}>
-            {tPopup('title')}
-          </h3>
-          <p className={styles['cancel-create-project-popup__text']}>
-            {tPopup('description')}
-          </p>
-        </div>
-        <div className={styles['cancel-create-project-popup__actions']}>
-          <Button color="green" variant="secondary-frame" onClick={onCancel}>
-            {tButtons('cancel')}
-          </Button>
-          <Button color="green" onClick={onConfirm}>
-            {tButtons('confirm')}
-          </Button>
-        </div>
-      </div>
-    </div>
+    <Dialog isOpen={isOpen} onClose={onCancel}>
+      <DialogHeader title={tPopup('title')} />
+      <DialogBody>{tPopup('description')}</DialogBody>
+      <DialogFooter>
+        <Button color="green" variant="secondary-frame" onClick={onCancel}>
+          {tButtons('cancel')}
+        </Button>
+        <Button color="green" onClick={onConfirm}>
+          {tButtons('confirm')}
+        </Button>
+      </DialogFooter>
+    </Dialog>
   );
 }
